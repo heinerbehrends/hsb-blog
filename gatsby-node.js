45067a95bd0e938c446b77489105ee6e42b378fb
heinerbehrends <heinerbehrends@hotmail.com>
2021-08-31 10:22:14 +0200

    removed keywords field from query

--- gatsby-node.js
+++ gatsby-node.js
@@ -22,7 +22,6 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
               slug
             }
             frontmatter {
-              keywords
               title
               description
             }
@@ -95,7 +94,6 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
   }
 
   const portfolioItems = portfolioResult.data.allMdx.nodes
-
   // Create blog posts pages
   // But only if there's at least one markdown file found at "content/blog" (defined in gatsby-config.js)
   // `context` is available in the template as a prop and as a variable in GraphQL
