a99a3c40bb72c2bbf686c0b92a143aa62a09a63f
heinerbehrends <heinerbehrends@hotmail.com>
2021-10-22 00:34:58 +0200

    added clickToLoad: true

--- src/components/stackBlitz.tsx
+++ src/components/stackBlitz.tsx
@@ -15,6 +15,7 @@ export default function StackBlitz({
   useEffect(() => {
     sdk.embedProjectId("elementOrId", projectId, {
       theme: "dark",
+      clickToLoad: true,
       openFile: "index.tsx",
       view,
       height,
